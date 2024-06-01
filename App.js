434036ce31f46a0ea16ca8fcf066cdfb1366cd3d
emmanuellaAk <emmanuellaanimwaa17@icloud.com>
2024-06-01 01:17:38 +0000

    searchbar components

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View,ScrollView  } from 'react-native';
+import Searchbar from './components/searchbar';
 
 export default function App() {
   return (
+    <ScrollView>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Searchbar/>
     </View>
+  </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: "flex-start", 
+    alignItems: "flex-start",
+    paddingTop: 52, 
+    padding: 20, 
+    backgroundColor:"#F7F0E8"
+  }
 });
